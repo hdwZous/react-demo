772654403738c5c3207871f54179aabf02eaa8ca
wangxinyao <wang_xy2@suixingpay.com>
2017-08-16 15:21:53 +0800

    fix fixedcontent

--- app/components/common/FixedContent.jsx
+++ app/components/common/FixedContent.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 let FixedContent = React.createClass({
 
     setFontSize () {
-        var designedWidth = 750
+        var ua = navigator.userAgent
+        var dpr = window.devicePixelRatio || 1;
+        var isIos = navigator.appVersion.match(/(iphone|ipad|ipod)/gi);
+        var matches = ua.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i);
+        var UCversion = ua.match(/U3\/((\d+|\.){5,})/i);
+        var isUCHd = UCversion && parseInt(UCversion[1].split('.').join(''), 10) >= 80;
+        if (!isIos && !(matches && matches[1] > 534) && !isUCHd) {
+            dpr = 1
+        }
+        var scale = 1 / dpr + 1
+        var designedWidth = 750 / scale
         let actualWidth = Math.min(450, $(window).width())
         document.documentElement.style.fontSize = actualWidth * 100 / designedWidth + 'px'
     },
