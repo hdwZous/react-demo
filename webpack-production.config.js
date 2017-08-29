30245a0090a6d9beb217b26c21fadeb7a4e0e52c
柴箐 <chaiqing@answern.com>
2017-08-29 19:06:41 +0800

    修改生产环境webpack配置信息

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -2,9 +2,12 @@
  * Created by chaiqing on 2017/7/26.
  */
 module.exports = require('./webpack-config')({
-    devServer: true,
-    hotComponents: true,
-    devtool: 'eval',
-    debug: true,
-    localRoute: true
+    devServer: false,
+    hotComponents: false,
+    longTermCaching: true,
+    minimize: true,
+    buildPath: 'build/react',
+    debug: false,
+    localRoute: false,
+    pathLink: 'https://weixin.95303.com/h5/public/'
 })
