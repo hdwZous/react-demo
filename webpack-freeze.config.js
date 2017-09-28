4550b2ba369ba8aaf10252943fd8e557da1bf0db
柴箐 <chaiqing@answern.com>
2017-09-28 16:23:59 +0800

    增加冻结命令

---
+++ webpack-freeze.config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chaiqing on 2017/7/26.
+ */
+module.exports = require('./webpack-config')({
+    devServer: false,
+    hotComponents: false,
+    longTermCaching: true,
+    minimize: true,
+    buildPath: 'build/react',
+    debug: false,
+    localRoute: false,
+    pathLink: 'https://freezeweixin.95303.com/h5/public/'
+})
